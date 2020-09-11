175cab4446ba739f783a59334f770851465c346b
Алексей <omaleks.ua@gmail.com>
2020-09-11 09:46:12 +0300

    last comments

---
+++ src/GitHubBasic/GitHubShotInstruction.java
@@ -0,0 +1,41 @@
+package GitHubBasic;
+
+public class GitHubShotInstruction {
+    public static void main(String[] args) {
+
+        /*
+         1. To create new Depository on GitHub:
+                Sign + (right up corner) - New repository - write Repository name - Create Repository
+
+         2. To create local git repository (to make our project under Git):
+                VCS - Import into version control - Create git repository - choose our project folder - OK
+
+         3. To first choosing of upload project folder (Add):
+                Idea project browser - select folders - RMB: Git - Add
+
+         4. To second choosing of upload project folder (Commit):
+                VCS - commit - check/uncheck folders (files) - write commit comments - commit
+
+         5. To copy URL GitHub repository:
+                GitHub - Main menu (right up conner) - Your repositories
+                LMB Repository name - Code open menu - url copy button
+
+         6. To point out GitHub upload repository URL:
+                VCS - Git - Push - origin - Define Remote - paste URL link - OK
+
+         7. To upload files (folders) to GitHub Repository:
+                VCS - Git - Push - Push
+
+         8. To download project from GitHub:
+                VCS - Checking for Version Control - Git - past URL GitHub Repository link (look at point 5) -
+                - clone (ore test)
+
+
+
+
+         */
+
+
+    }
+
+}
