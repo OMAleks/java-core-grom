6f077aa6234cd5d0a13814f5f8a4e55151bae608
Алексей <omaleks.ua@gmail.com>
2020-09-14 07:34:56 +0300

    shot git integration instruction comments

---
+++ src/GitHubBasic/GitAndIdeaIntegration.java
@@ -0,0 +1,45 @@
+package GitHubBasic;
+
+public class GitAndIdeaIntegration {
+
+    public static void main(String[] args) {
+        System.out.println("How to integrate Git into Idea");
+    }
+
+
+    /*
+                                           01 GitHub Registration
+
+                              Follow instruction on  https://github.com/join
+
+
+                                           02 Git download
+
+                                   https://git-scm.com/downloads
+
+
+                                           03 Git Installation
+
+              All default. We must  know Git folder path. For example: C:\Program Files\Git\
+
+
+
+                                           04 Git Integration
+
+        A) Open Idea.
+        B) Set git executable file path: File - Setting - Version Control - Git.
+            For example: C:\Program Files\Git\bin\git.exe
+        C) Test integration: Test.
+        D) Press OK to finish.
+
+                                            05 GitHub Connection
+
+             File - Setting - Version Control - GitHub - type GitHub Login and Password  - Log IN - Ok
+
+
+
+     */
+
+
+
+}
